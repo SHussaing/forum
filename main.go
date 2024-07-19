4aa81bc05e4e56119bc6e1397a3e081ae769989c
SHussaing <husain187@hotmail.com>
2024-07-19 21:09:58 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	handlers "forum/Handlers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Define the directory to serve static files from
+	http.Handle("/Static/", http.StripPrefix("/Static/", http.FileServer(http.Dir("Static"))))
+
+	// Define the routes
+	http.HandleFunc("/", handlers.IndexHandler)
+	http.HandleFunc("/Register", handlers.RegisterHandler)
+
+	fmt.Println("Server started at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
